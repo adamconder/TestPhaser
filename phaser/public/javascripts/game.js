45f7ed0b764ee677bf5302c0fe529d0e2968fd76
Adam Conder <adamdconder@gmail.com>
2018-02-20 12:09:11 +0000

    Adding dino assets

--- phaser/public/javascripts/game.js
+++ phaser/public/javascripts/game.js
@@ -5,6 +5,8 @@
     var width = window.innerWidth;
     var game = configure();
 
+    var vita;
+
     function configure() {
         var game = new Phaser.Game(
             width,
@@ -17,16 +19,32 @@
     }
 
     function preload() {
-        game.load.image('logo', 'images/phaser.png');
+        // game.load.image('logo', 'images/phaser.png');
+        game.load.spritesheet('vita', 'images/dino/vita.png', 24, 24);
     }
 
     function create() {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        // var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        // logo.anchor.setTo(0.5, 0.5);
+
+        addDinos();
+    }
+
+    function addDinos() {
+        vita = game.add.sprite(game.world.centerX, game.world.centerY, 'vita');
+        vita.anchor.setTo(0.5, 0.5);
+        vita.animations.add('walk');
+        vita.animations.play('walk', 50, true);
+        game.add.tween(vita).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
     }
 
     function update() {
-        console.log('updating')
+
+        if (vita.x >= 300) {
+            vita.scale.x += 0.01;
+            vita.scale.y += 0.01;
+        }
+
     }
 
 })();
